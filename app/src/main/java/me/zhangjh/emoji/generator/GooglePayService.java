f6de17c8a962e59e04465d89cc1c71235f8d7803
zhangjh451 <zhangjh451@midea.com>
2023-10-06 21:15:54 +0800

    内购付费服务

--- app/src/main/java/me/zhangjh/emoji/generator/GooglePayService.java
+++ app/src/main/java/me/zhangjh/emoji/generator/GooglePayService.java
@@ -3,6 +3,7 @@ package me.zhangjh.emoji.generator;
 import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -35,16 +36,16 @@ public class GooglePayService {
     }
 
     public void getClient(Context context) {
-        if(billingClient == null) {
+        if (billingClient == null) {
             PurchasesUpdatedListener purchasesUpdatedListener = (billingResult, purchases) -> {
                 // success
-                if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK
                         && CollectionUtils.isNotEmpty(purchases)) {
                     for (Purchase purchase : purchases) {
                         // handlePurchase
                         handlePurchase.apply(purchase);
                     }
-                } else if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.USER_CANCELED) {
+                } else if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.USER_CANCELED) {
                     // Handle an error caused by a user cancelling the purchase flow.
 
                 } else {
@@ -68,12 +69,13 @@ public class GooglePayService {
                 // 断了重连
                 billingClient.startConnection(this);
             }
+
             @Override
             public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
                 // Try to restart the connection on the next request to
                 // Google Play by calling the startConnection() method.
                 System.out.println(JSONObject.toJSONString(billingResult));
-                if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
                     launch();
                 }
             }
@@ -93,23 +95,30 @@ public class GooglePayService {
 
     public void launch() {
         billingClient.queryProductDetailsAsync(this.queryItems(),
-            (billingResult, list) -> {
-                // process returned productDetailsList
-                for (ProductDetails details : list) {
-                    ImmutableList<BillingFlowParams.ProductDetailsParams> productDetailsParamList =
-                            ImmutableList.of(BillingFlowParams.ProductDetailsParams.newBuilder()
-                                    .setProductDetails(details)
-                                    .build()
-                    );
-                    BillingFlowParams flowParams = BillingFlowParams.newBuilder()
-                            .setProductDetailsParamsList(productDetailsParamList)
-                            .build();
-                    BillingResult flowLaunchResult = billingClient.launchBillingFlow(activity, flowParams);
-                    if(flowLaunchResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-                        // 发起购买流程成功
-                        Log.d("google pay", "start launch flow success");
+                (billingResult, list) -> {
+                    Log.d("query product", JSONObject.toJSONString(billingResult));
+                    if (CollectionUtils.isEmpty(list)) {
+                        activity.runOnUiThread(() ->
+                                Toast.makeText(activity.getApplicationContext(),
+                                        R.string.query_product_failed, Toast.LENGTH_SHORT).show());
+                        return;
                     }
-                }
-            });
+                    // process returned productDetailsList
+                    for (ProductDetails details : list) {
+                        ImmutableList<BillingFlowParams.ProductDetailsParams> productDetailsParamList =
+                                ImmutableList.of(BillingFlowParams.ProductDetailsParams.newBuilder()
+                                        .setProductDetails(details)
+                                        .build()
+                                );
+                        BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                                .setProductDetailsParamsList(productDetailsParamList)
+                                .build();
+                        BillingResult flowLaunchResult = billingClient.launchBillingFlow(activity, flowParams);
+                        if (flowLaunchResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                            // 发起购买流程成功
+                            Log.d("google pay", "start launch flow success");
+                        }
+                    }
+                });
     }
 }
